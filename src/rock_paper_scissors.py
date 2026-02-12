816d24785061f13670ecaaf6c004614728610def
Miguel <mpahinogutierrezpahino@gmail.com>
2026-02-12 10:42:53 +0100

    feat(rock_paper_scissors): :triangular_flag_on_post: cambiar el funcionamiento de get _user_action

--- src/rock_paper_scissors.py
+++ src/rock_paper_scissors.py
@@ -16,98 +16,87 @@ class GameResult(IntEnum):
     Tie = 2
 
 
-Victories = {
-    GameAction.Rock: GameAction.Paper,
-    GameAction.Paper: GameAction.Scissors,
-    GameAction.Scissors: GameAction.Rock
-}
+
+class Game:
+
+    def __init__(self):
+        self.victories = {
+        GameAction.Rock: GameAction.Paper,
+        GameAction.Paper: GameAction.Scissors,
+        GameAction.Scissors: GameAction.Rock
+    }
 
 
-def assess_game(user_action, computer_action):
-    game_result = None
+    def assess_game(self, user_action, computer_action):
+        game_result = None
 
-    if user_action == computer_action:
-        print(f"User and computer picked {user_action.name}. Draw game!")
-        game_result = GameResult.Tie
+        if user_action == computer_action:
+            print(f"User and computer picked {user_action.name}. Draw game!")
+            game_result = GameResult.Tie
 
-    # You picked Rock
-    elif user_action == GameAction.Rock:
-        if computer_action == GameAction.Scissors:
-            print("Rock smashes scissors. You won!")
-            game_result = GameResult.Victory
-        else:
-            print("Paper covers rock. You lost!")
-            game_result = GameResult.Defeat
+        # You picked Rock
+        elif user_action == GameAction.Rock:
+            if computer_action == GameAction.Scissors:
+                print("Rock smashes scissors. You won!")
+                game_result = GameResult.Victory
+            else:
+                print("Paper covers rock. You lost!")
+                game_result = GameResult.Defeat
 
-    # You picked Paper
-    elif user_action == GameAction.Paper:
-        if computer_action == GameAction.Rock:
-            print("Paper covers rock. You won!")
-            game_result = GameResult.Victory
-        else:
-            print("Scissors cuts paper. You lost!")
-            game_result = GameResult.Defeat
+        # You picked Paper
+        elif user_action == GameAction.Paper:
+            if computer_action == GameAction.Rock:
+                print("Paper covers rock. You won!")
+                game_result = GameResult.Victory
+            else:
+                print("Scissors cuts paper. You lost!")
+                game_result = GameResult.Defeat
 
-    # You picked Scissors
-    elif user_action == GameAction.Scissors:
-        if computer_action == GameAction.Rock:
-            print("Rock smashes scissors. You lost!")
-            game_result = GameResult.Defeat
-        else:
-            print("Scissors cuts paper. You won!")
-            game_result = GameResult.Victory
+        # You picked Scissors
+        elif user_action == GameAction.Scissors:
+            if computer_action == GameAction.Rock:
+                print("Rock smashes scissors. You lost!")
+                game_result = GameResult.Defeat
+            else:
+                print("Scissors cuts paper. You won!")
+                game_result = GameResult.Victory
 
-    return game_result
-
-
-def get_computer_action(user_actions_history, game_history):
-    # No previous user actions => random computer choice
-    if not user_actions_history or not game_history:
-        computer_action = get_random_computer_action()
-    # Basic AI functionality
-    # 1) If the user won the last round, he may repeat the last choice
-    # 2) If the user lost the last round, he may change to the next action in the sequence
-    # 3) If user and computer tied in the last round, then get a random computer choice
-    else:
-        # Path 1)
-        if game_history[-1] == GameResult.Victory:
-            computer_action = get_winner_action(user_actions_history[-1])
-        # Path 2)
-        elif game_history[-1] == GameResult.Defeat:
-            computer_action = get_winner_action(GameAction((user_actions_history[-1].value + 1) % len(GameAction)))
-        # Random choice
-        else:
-            computer_action = get_random_computer_action()
-
-    print(f"Computer picked {computer_action.name}.")
-
-    return computer_action
+        return game_result
 
 
-def get_user_action():
-    # Scalable to more options (beyond rock, paper and scissors...)
-    game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
-    game_choices_str = ", ".join(game_choices)
-    user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
-    user_action = GameAction(user_selection)
+    def get_computer_action():
+        
+        valores = [accion.value for accion in GameAction]
+        computer_action = random.choice(valores)
 
-    return user_action
+        return computer_action
 
 
-def get_random_computer_action():
-    computer_selection = random.randint(0, len(GameAction) - 1)
-    computer_action = GameAction(computer_selection)
+    def get_user_action():
+        # Scalable to more options (beyond rock, paper and scissors...)
+        game_choices = [f"{game_action.name}[{game_action.value}]" for game_action in GameAction]
+        game_choices_str = ", ".join(game_choices)
+        user_selection = int(input(f"\nPick a choice ({game_choices_str}): "))
+        user_action = GameAction(user_selection)
 
-    return computer_action
+        return user_action
 
 
-def get_winner_action(game_action):
-    return Victories[game_action]
+    def get_random_computer_action():
+        computer_selection = random.randint(0, len(GameAction) - 1)
+        computer_action = GameAction(computer_selection)
+
+        return computer_action
 
 
-def play_another_round():
-    another_round = input("\nAnother round? (y/n): ")
-    return another_round.lower() == 'y'
+    def get_winner_action(game_action):
+        return Victories[game_action]
+
+
+    def play_another_round():
+        another_round = input("\nAnother round? (y/n): ")
+        return another_round.lower() == 'y'
+
 
 
 def main():
